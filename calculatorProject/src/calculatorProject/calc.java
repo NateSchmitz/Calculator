5d87b58a28403f130ebc0065283ebe7a19bedb5f
Nate <Nate@DESKTOP-G67OSHC>
2018-11-07 16:15:56 -0600

    added first pane to open

--- calculatorProject/src/calculatorProject/calc.java
+++ calculatorProject/src/calculatorProject/calc.java
@@ -1,9 +1,10 @@
 package calculatorProject;
+import javax.swing.JOptionPane;
 
 public class calc {
 	public static void main(String[] args) {
 		String[] opCodes = { "Multiplication", "Division", "Addition", "Subtratcion", "Square Root" };
-		System.out.println("Hello World");
-		System.out.println("How is your world");
+		System.out.println("This is my calculator console");
+		JOptionPane .showMessageDialog(null, "message", "title", JOptionPane.ERROR_MESSAGE);
 	}
 }
