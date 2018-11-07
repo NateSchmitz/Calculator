6a4033c3ccfcc5c66f2e60cec3e9ba78642a57fb
Nate <Nate@DESKTOP-G67OSHC>
2018-11-07 13:35:42 -0600

    added option pane

--- calculatorProject/src/calculatorProject/calc.java
+++ calculatorProject/src/calculatorProject/calc.java
@@ -1,6 +1,9 @@
 package calculatorProject;
 
 public class calc {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
+		String[] opCodes = { "Multiplication", "Division", "Addition", "Subtratcion", "Square Root" };
+		System.out.println("Hello World");
+		System.out.println("How is your world");
 	}
 }
