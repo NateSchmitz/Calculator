fb2f7fdc93542f151041453c5714df26a7cfdbbf
NateSchmitz <nateschmitz17@gmail.com>
2018-11-07 19:57:54 -0600

    Update calc.java

--- calculatorProject/src/calculatorProject/calc.java
+++ calculatorProject/src/calculatorProject/calc.java
@@ -5,7 +5,7 @@ public class calc {
 	public static void main(String[] args) {
 		String[] opCodes = { "Multiplication", "Division", "Addition", "Subtratcion", "Square Root" };
 		System.out.println("This is my calculator console");
-		JOptionPane .showMessageDialog(null, "message", "title", JOptionPane.ERROR_MESSAGE);
+		JOptionPane .showMessageDialog(null, "message1", "title", JOptionPane.ERROR_MESSAGE);
 		JOptionPane.showMessageDialog(null, "message2", "title" , JOptionPane.INFORMATION_MESSAGE );
 	}
 }
