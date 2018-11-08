b3fb548c96e5ee92b6976539eb27816253de6baf
nate <nate@DESKTOP-336D6HO>
2018-11-07 19:56:58 -0600

    Added message 2

--- calculatorProject/src/calculatorProject/calc.java
+++ calculatorProject/src/calculatorProject/calc.java
@@ -6,5 +6,6 @@ public class calc {
 		String[] opCodes = { "Multiplication", "Division", "Addition", "Subtratcion", "Square Root" };
 		System.out.println("This is my calculator console");
 		JOptionPane .showMessageDialog(null, "message", "title", JOptionPane.ERROR_MESSAGE);
+		JOptionPane.showMessageDialog(null, "message2", "title" , JOptionPane.INFORMATION_MESSAGE );
 	}
 }
